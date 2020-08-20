6ac41c6a1322074a8cbd39542b2b293253584c9b
Victor <victorgutierrezgomes@gmail.com>
2020-08-20 14:53:07 -0400

    Merge pull request #2 from future4code/codigoAle
    Codigo ale

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
 import dotenv from "dotenv";
 import express from "express";
-import { AddressInfo } from "net";
-import { Signup } from "../endpoints/Signup";
+import {AddressInfo} from "net";
+import {Signup} from "../endpoints/Signup";
+import {login} from "../endpoints/Login";
+
+
+ 
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 
 app.post("/user/signup", Signup);
+app.post("/login", login);
+
 
 const server = app.listen(process.env.PORT || 3003, () => {
     if (server) {
